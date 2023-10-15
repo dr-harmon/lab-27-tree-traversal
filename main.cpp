aedc23d6e1fa69f248c539341ca33df693e65090
Trevor Harmon <tharmon@laspositascollege.edu>
2023-10-15 16:29:37 -0700

    Add solution

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include "tree.h"
+#include "document.h"
+#include "corporation.h"
+#include "expression.h"
 
 #include <cstdlib>
 #include <iostream>
@@ -7,5 +10,28 @@ using namespace std;
 
 int main()
 {
+	auto document = makeDocumentTree();
+	traversePreorder<string>(document, [](const string& s) {
+		cout << s << endl;
+	});
+
+	auto corporation = makeCorporationTree();
+	traversePostorder<string>(corporation, [](const string& s) {
+		cout << s << endl;
+	});
+
+	auto expression = makeExpressionTree();
+	traverseInorder<shared_ptr<Term>>(expression,
+		[]() {
+			cout << "(";
+		},
+		[](const shared_ptr<Term>& term) {
+			cout << term->toString() << " ";
+		},
+		[]() {
+			cout << ")";
+		}
+	);
+
 	return EXIT_SUCCESS;
 }
--- src/tree.h
+++ src/tree.h
@@ -15,15 +15,51 @@ struct Tree {
 };
 
 template <typename E>
+void traversePreorder(Node<E>& node, std::function<void(const E&)> visit)
+{
+    visit(node.e);
+    for (auto& node : node.children) {
+        traversePreorder(node, visit);
+    }
+}
+
+template <typename E>
 void traversePreorder(Tree<E>& tree, std::function<void(const E&)> visit)
 {
-    // TODO
+    traversePreorder(tree.root, visit);
+}
+
+template <typename E>
+void traversePostorder(const Node<E>& node, std::function<void(const E&)> visit)
+{
+    for (auto& node : node.children) {
+        traversePostorder(node, visit);
+    }
+    visit(node.e);
 }
 
 template <typename E>
 void traversePostorder(const Tree<E>& tree, std::function<void(const E&)> visit)
 {
-    // TODO
+    traversePostorder(tree.root, visit);
+}
+
+template <typename E>
+void traverseInorder(const Node<E>& node,
+    std::function<void(void)> beginTraversal,
+    std::function<void(const E&)> visit,
+    std::function<void(void)> endTraversal)
+{
+    bool isInternal = !node.children.empty();
+    if (isInternal) {
+        beginTraversal();
+        traverseInorder(node.children[0], beginTraversal, visit, endTraversal);
+    }
+    visit(node.e);
+    if (isInternal) {
+        traverseInorder(node.children[1], beginTraversal, visit, endTraversal);
+        endTraversal();
+    }
 }
 
 template <typename E>
@@ -32,5 +68,5 @@ void traverseInorder(const Tree<E>& tree,
     std::function<void(const E&)> visit,
     std::function<void(void)> endTraversal)
 {
-    // TODO
+    traverseInorder(tree.root, beginTraveral, visit, endTraversal);
 }
